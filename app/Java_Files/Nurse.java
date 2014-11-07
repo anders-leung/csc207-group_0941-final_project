6ef7c50df489d1fac4deeea1efd616195694b277
c3linchu <c3linchu@480b8e0a-87bc-4c1d-9d3f-f9567d63173d>
2014-11-07 20:37:30 +0000

    added Nurse.java

---
+++ app/Java_Files/Nurse.java
@@ -0,0 +1,62 @@
+package com.example.triage;
+
+
+import java.util.ArrayList;
+import java.util.Map;
+
+
+public class Nurse {
+	
+	protected Patient patient;
+	protected Map<String, Patient> patientlist;
+	
+
+	public Nurse() {
+	}
+	
+	/**
+	 * Return a certain patient given by the health card number from the patient list
+	 * else return null
+	 * @param patientlist
+	 * @param hcn
+	 * @return
+	 */
+	public Patient lookupPatient(Map<String, Patient> patientlist, String hcn){
+		
+		if (patientlist.get(hcn) != null) {
+			patient = patientlist.get(hcn);
+			return patient;
+		}
+		else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Nurse can update a patient's visit record with vital signs at a particular
+	 * time, retaining older values
+	 */
+	public void updatepatient(String toa, String vitaltime, double temp, 
+			int bloodpressure, int heartrate) {
+		this.patient.setVitalsigns(toa, vitaltime, temp, bloodpressure, heartrate);
+	}
+	
+	/**
+	 * Nurse can create a new visit record based on the patient's arrival time
+	 */
+	public void newVisitRecord(String toa, String vitaltime, double temp, 
+			int bloodpressure, int heartrate) {
+		this.patient.setVitalsigns(toa, vitaltime, temp, bloodpressure, heartrate);
+	}
+	
+	/**
+	 * Nurse can view previous record for a given patient
+	 * @param patientlist
+	 * @param toa
+	 * @return
+	 */
+	public Map<String, ArrayList<Number>> lookupPatientRecord(Map<String, Patient> patientlist, 
+			String toa) {
+		return this.patient.getVitalsigns().get(toa);
+	}
+}
